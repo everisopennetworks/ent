f902048d2461fc56761e6763cd00f463519a3c61
W0lfer01 <aitorgp101@gmail.com>
2024-11-15 15:53:26 +0100

    remove unique for OrderFieldName

--- entc/gen/type.go
+++ entc/gen/type.go
@@ -1995,9 +1995,9 @@ func (e Edge) OrderTermsName() (string, error) {
 
 // OrderFieldName returns the function/option name for ordering by edge field.
 func (e Edge) OrderFieldName() (string, error) {
-	if !e.Unique {
-		return "", fmt.Errorf("edge %q is not-unique", e.Name)
-	}
+	// if !e.Unique {
+	// 	return "", fmt.Errorf("edge %q is not-unique", e.Name)
+	// }
 	return fmt.Sprintf("By%sField", pascal(e.Name)), nil
 }
 
